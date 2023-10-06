0c0003b2890fe08927d041665179eec053d49513
tracer-ics2023 <tracer@njuics.org>
2023-10-06 22:10:08 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:10:08 up 0 min,  1 user,  load average: 1.61, 0.47, 0.16

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -33,6 +33,11 @@ static char *code_format =
 
 static void gen_rand_expr() {
   buf[0] = '\0';
+	switch(choose(3)){
+		case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+	}
 }
 
 int main(int argc, char *argv[]) {
