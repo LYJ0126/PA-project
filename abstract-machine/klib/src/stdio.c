b6b9c9da3ac50180b55dd3ed4413509e40d9d15a
tracer-ics2023 <tracer@njuics.org>
2023-10-28 16:13:25 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:13:25 up  4:38,  3 users,  load average: 0.00, 0.01, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,38 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int itoa(int n, char *str, uint32_t radix)
+{
+	assert(radix<=16 && radix>=2);
+	uint32_t unum;
+	int i=0,rem,k;
+	if(radix==10&&n<0)//十进制数并且是负数
+  {
+      unum=(unsigned)-n;//将n的绝对值赋给unum
+      str[i++]='-';//在字符串最前面设置为'-'号，并且索引加1
+  }
+  else unum=(unsigned)n;//若是n为正，直接赋值给unum
+	//转换
+	do{
+		rem = unum%radix;
+		str[i++] = (rem>=10)?'a'+rem-10:'0'+rem;
+		unum /= radix;
+	}while(unum);
+	str[i]='\0';
+	//调换顺序
+	if(str[0] == '-') k=1;
+	else k=0;
+	char temp;
+	int j=i-1;
+	while(k<j){
+		temp = str[k];
+		str[k] = str[j];
+		str[j] = temp;
+		k++;
+		j--;
+	}
+	return i;
+}
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +46,32 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	va_start(ap, fmt);
+	char *s = out;
+	while(*fmt!='\0'){
+		if(*fmt != '%'){
+			*out++ = *fmt;
+		}
+		else {
+			fmt++;
+			switch(*fmt){
+				case 'd': out += itoa(va_arg(ap, int), out, 10); break;
+				case 'i': out += itoa(va_arg(ap, int), out, 10); break;
+				case '%': *out++ = *fmt; break;
+				case 's': {
+										char *temp = va_arg(ap, char*);
+										strcpy(out, temp);
+										out += strlen(out);
+										break;
+									}
+			}
+		}
+		fmt++;
+	}
+	*out = '\0';
+	va_end(ap);
+	return out-s;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
