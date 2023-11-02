a932916525b8d26e6a5d43f9d62031f9a0645f14
tracer-ics2023 <tracer@njuics.org>
2023-11-03 01:15:47 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:15:47 up 9 min,  1 user,  load average: 0.08, 0.14, 0.13

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -3,40 +3,36 @@
 #include <klib-macros.h>
 #include <stdarg.h>
 
-#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)//没有定义使用native
 
-int itoa(int n, char *str, uint32_t radix)
+uint32_t itoa(uint32_t n, char* str, uint32_t radix, int upper, int sign)//整数转字符串(不限制是有符号还是无符号)
 {
-	assert(radix<=16 && radix>=2);
-	uint32_t unum;
-	int i=0,rem,k;
-	if(radix==10&&n<0)//十进制数并且是负数
-  {
-      unum=(unsigned)-n;//将n的绝对值赋给unum
-      str[i++]='-';//在字符串最前面设置为'-'号，并且索引加1
-  }
-  else unum=(unsigned)n;//若是n为正，直接赋值给unum
-	//转换
-	do{
-		rem = unum%radix;
-		str[i++] = (rem>=10)?'a'+rem-10:'0'+rem;
-		unum /= radix;
-	}while(unum);
-	str[i]='\0';
-	//调换顺序
-	if(str[0] == '-') k=1;
-	else k=0;
+	assert(radix <= 16 && radix >= 2);
+	int i = 0, rem, k = 0;
+	if (radix == 10  && sign==1 && (n >> 31)) {//十进制有符号数且为负数
+		n = ~n + 1;//取反加一求原码
+		str[i++] = '-';//字符串最前面设置负号且下标加一
+	}
+	do {
+		rem = n % radix;
+		str[i++] = (rem >= 10) ? ((upper == 1) ? 'A' + rem - 10 : 'a' + rem - 10) : '0' + rem;
+		n /= radix;
+	} while (n);
+	str[i] = '\0';
+	//反序
+	if (str[0] == '-') k = 1;
 	char temp;
-	int j=i-1;
-	while(k<j){
+	int j = i - 1;
+	while (k < j) {
 		temp = str[k];
 		str[k] = str[j];
 		str[j] = temp;
-		k++;
-		j--;
+		++k;
+		--j;
 	}
 	return i;
 }
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -45,33 +41,37 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int sprintf(char* out, const char* fmt, ...)
+{
+	char* start = out;
 	va_list ap;
 	va_start(ap, fmt);
-	char *s = out;
-	while(*fmt!='\0'){
-		if(*fmt != '%'){
-			*out++ = *fmt;
-		}
+	while (*fmt != '\0') {
+		if (*fmt != '%') *out++ = *fmt;
 		else {
 			fmt++;
-			switch(*fmt){
-				case 'd': out += itoa(va_arg(ap, int), out, 10); break;
-				case 'i': out += itoa(va_arg(ap, int), out, 10); break;
-				case '%': *out++ = *fmt; break;
-				case 's': {
-										char *temp = va_arg(ap, char*);
-										strcpy(out, temp);
-										out += strlen(out);
-										break;
-									}
+			switch (*fmt) {
+			case 'd': out += itoa(va_arg(ap, int), out, 10, 0, 1); break;
+			case 'i': out += itoa(va_arg(ap, int), out, 10, 0, 1); break;
+			case 'u': out += itoa(va_arg(ap, uint32_t), out, 10, 0, 0); break;
+			case 'o': out += itoa(va_arg(ap, uint32_t), out, 8, 0, 0); break;
+			case 'x': out += itoa(va_arg(ap, uint32_t), out, 16, 0, 0); break;
+			case 'X': out += itoa(va_arg(ap, uint32_t), out, 16, 1, 0); break;
+			case 'c': *out++ = va_arg(ap,int); break;
+			case '%': *out++ = va_arg(ap,int); break;
+			case 's': {
+				char* temp = va_arg(ap, char*); break;
+				strcpy(out, temp);
+				out += strlen(out);
+				break;
+			}
 			}
 		}
 		fmt++;
 	}
 	*out = '\0';
 	va_end(ap);
-	return out-s;
+	return out - start;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
