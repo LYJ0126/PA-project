57be7fae7ab6026254d30912074f30c05ed51d29
tracer-ics2023 <tracer@njuics.org>
2023-11-15 21:12:31 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:31 up 0 min,  1 user,  load average: 0.47, 0.14, 0.05

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -38,7 +38,7 @@ void *malloc(size_t size) {
 	size  = (size_t)ROUNDUP(size, 8);
   char *old = myhbrk;
   myhbrk += size;
-	printf("%u  %u  %u\n",heap.start,myhbrk,heap.end);
+	printf("%x  %x  %x\n",heap.start,myhbrk,heap.end);
   assert((uintptr_t)heap.start <= (uintptr_t)myhbrk && (uintptr_t)myhbrk < (uintptr_t)heap.end);
   for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)myhbrk; p ++) {
     *p = 0;
