908ac00d19b757bbf1ad68ad657111b37b9986f9
tracer-ics2023 <tracer@njuics.org>
2023-11-23 18:35:18 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:35:18 up  2:02,  1 user,  load average: 0.51, 0.34, 0.18

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -75,7 +75,7 @@ void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
 	uint32_t sync = vgactl_port_base[1];//AM中有SYNC_ADDR=VGACTL_ADDR+4
-  //printf("bgactl_port_base: %u,%u\n",vgactl_port_base[0],vgactl_port_base[1]);
+  //printf("vgactl_port_base: %u,%u\n",vgactl_port_base[0],vgactl_port_base[1]);
 	if(sync>0){
 		update_screen();
 		vgactl_port_base[1]=0;
