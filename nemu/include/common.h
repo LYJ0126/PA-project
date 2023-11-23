5ab6459a759b6f3eebab4d982477adc97511a6e8
tracer-ics2023 <tracer@njuics.org>
2023-11-23 20:49:31 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:49:31 up  4:16,  1 user,  load average: 0.37, 0.29, 0.16

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -35,7 +35,6 @@
 #define PMEM64 1
 #endif
 
-//volatile static int dummy;
 typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) word_t;//word
 typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t)  sword_t;//signed word
 #define FMT_WORD MUXDEF(CONFIG_ISA64, "0x%016" PRIx64, "0x%08" PRIx32)
