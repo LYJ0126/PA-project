30ad7b58730a44efd81260e490b285bf7d5a916f
tracer-ics2023 <tracer@njuics.org>
2023-12-15 22:23:54 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:23:54 up  1:04,  1 user,  load average: 0.15, 0.17, 0.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,13 +2,14 @@
 void do_syscall(Context*);
 
 static Context* do_event(Event e, Context* c) {
+  printf("event ID = %d\n", e.event);
   switch (e.event) {
     case EVENT_YIELD:printf("Event yield, event ID = %d\n", e.event); break;
     case EVENT_SYSCALL:
     printf("Event syscall, event ID = %d\n", e.event);
-    printf("a7:%d,a0:%d,a1:%d,a2:c%d,a0:%d\n",c->GPR1,c->GPR2,c->GPR3,c->GPR4,c->GPRx);
+    printf("a7:%d,a0:%d,a1:%d,a2:%d,a0:%d\n",c->GPR1,c->GPR2,c->GPR3,c->GPR4,c->GPRx);
     do_syscall(c);
-    printf("a7:%d,a0:%d,a1:%d,a2:c%d,a0:%d\n",c->GPR1,c->GPR2,c->GPR3,c->GPR4,c->GPRx);
+    printf("a7:%d,a0:%d,a1:%d,a2:%d,a0:%d\n",c->GPR1,c->GPR2,c->GPR3,c->GPR4,c->GPRx);
     break;
     default: panic("Unhandled event ID = %d", e.event);
   }
