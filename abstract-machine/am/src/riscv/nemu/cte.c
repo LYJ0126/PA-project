81608bfa16f92b8e17528b014d9e0676e19a70a0
tracer-ics2023 <tracer@njuics.org>
2023-12-15 22:48:45 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:48:45 up  1:29,  1 user,  load average: 0.66, 0.39, 0.26

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,31 +5,49 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  for(int i = 0; i < 32; i++){
+  /*for(int i = 0; i < 32; i++){
       printf("%x ", c->gpr[i]);
   }
     printf("\n");
     printf("mcause: %x\n", c->mcause);
     printf("mstatus: %x\n", c->mstatus);
-    printf("mepc: %x\n", c->mepc);
+    printf("mepc: %x\n", c->mepc);*/
   if (user_handler) {
     Event ev = {0};
     printf("c->mcause: %d\n", c->mcause);
     switch (c->mcause) {
       case 0: ev.event = EVENT_SYSCALL; break;
       case 1: ev.event = EVENT_SYSCALL; break;
+      case 2: ev.event = EVENT_SYSCALL; break;
+      case 3: ev.event = EVENT_SYSCALL; break;
+      case 4: ev.event = EVENT_SYSCALL; break;
+      case 5: ev.event = EVENT_SYSCALL; break;
+      case 6: ev.event = EVENT_SYSCALL; break;
+      case 7: ev.event = EVENT_SYSCALL; break;
+      case 8: ev.event = EVENT_SYSCALL; break;
+      case 9: ev.event = EVENT_SYSCALL; break;
+      case 10: ev.event = EVENT_SYSCALL; break;
+      case 11: ev.event = EVENT_SYSCALL; break;
+      case 12: ev.event = EVENT_SYSCALL; break;
+      case 13: ev.event = EVENT_SYSCALL; break;
+      case 14: ev.event = EVENT_SYSCALL; break;
+      case 15: ev.event = EVENT_SYSCALL; break;
+      case 16: ev.event = EVENT_SYSCALL; break;
+      case 17: ev.event = EVENT_SYSCALL; break;
+      case 18: ev.event = EVENT_SYSCALL; break;
+      case 19: ev.event = EVENT_SYSCALL; break;
       case 0xffffffff: ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
-    for(int i = 0; i < 32; i++){
+    /*for(int i = 0; i < 32; i++){
       printf("%x ", c->gpr[i]);
   }
     printf("\n");
     printf("mcause: %x\n", c->mcause);
     printf("mstatus: %x\n", c->mstatus);
-    printf("mepc: %x\n", c->mepc);
+    printf("mepc: %x\n", c->mepc);*/
     assert(c != NULL);
   }
   return c;
