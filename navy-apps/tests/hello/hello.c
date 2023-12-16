c3c0a36a630345cb2c748bf5762a823f88f17351
tracer-ics2023 <tracer@njuics.org>
2023-12-16 21:24:36 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:36 up 26 min,  1 user,  load average: 0.22, 0.28, 0.25

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 int main() {
-  //write(1, "Hello World!\n", 13);
+  write(1, "Hello World!\n", 13);
+  write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
   while (1) {
     j ++;
-    if (j == 1000000000) {
-      //write(1, "Hello World!\n", 13);
+    if (j == 1000000) {
+      write(1, "Hello World!\n", 13);
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
       j = 0;
     }
