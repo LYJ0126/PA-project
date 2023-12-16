f293384d308208df712091ceb040b83aefed4ec9
tracer-ics2023 <tracer@njuics.org>
2023-12-16 12:18:57 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:18:57 up 53 min,  1 user,  load average: 0.04, 0.23, 0.23

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,10 +3,23 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;//a7
+  a[1] = c->GPR2;//a0
+  a[2] = c->GPR3;//a1
+  a[3] = c->GPR4;//a2
   a[4] = c->GPRx;//a0
   switch (a[0]) {
     case 0:printf("SYS_exit, a0 = %d\n", a[4]); halt(a[4]); break;
     case 1: printf("SYS_yield\n"); yield(); c->GPRx = 0; break;
+    case 4: 
+    printf("SYS_write, a0 = %d, a1 = %x, a2 = %d\n", a[1], a[2], a[3]);
+    if(a[1] == 1 || a[1] == 2) {//stdout or stderr
+      char *buf = (char *)a[2];
+      for(int i = 0; i < a[3]; i++) {//a[3]是写入的字节数
+        putch(buf[i]);
+      }
+    }
+    c->GPRx = a[3];
+    break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
