02947241bcff9ee1d8669d0096e6cd8c42e7edaa
tracer-ics2023 <tracer@njuics.org>
2023-12-17 20:41:35 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:41:35 up 25 min,  1 user,  load average: 0.03, 0.19, 0.32

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,12 +16,12 @@ extern size_t fs_read(int fd, void *buf, size_t len);
 //size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
-  
+  /*
   //从文件中读取ELF头
   Elf_Ehdr elf;
   //printf("offset:0,len:%d,ramdisk_size:%x\n",sizeof(Elf_Ehdr),get_ramdisk_size());
   ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
-  //assert(elf.e_ident[0] == 0x7f && elf.e_ident[1] == 'E' && elf.e_ident[2] == 'L' && elf.e_ident[3] == 'F');//0x7fELF
+  assert(elf.e_ident[0] == 0x7f && elf.e_ident[1] == 'E' && elf.e_ident[2] == 'L' && elf.e_ident[3] == 'F');//0x7fELF
   // 读取program header，并加载到内存中
   for (int i = 0; i < elf.e_phnum; i++) {
     Elf_Phdr phdr;
@@ -33,8 +33,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   }
   //跳转到程序入口执行
   return elf.e_entry;
-  
-  /*printf("filename:%s\n",filename);
+  */
+  printf("filename:%s\n",filename);
   int fd = fs_open(filename);
   printf("fd:%d\n",fd);
   Elf_Ehdr elf;
@@ -47,7 +47,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       fs_read(fd, (void *)phdr.p_vaddr, phdr.p_memsz);
       memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
     }
-  }*/
+  }
   return elf.e_entry;
 }
 
