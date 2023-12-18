79348e3e477ffbeb977451435b60b6f66dc8ecf4
tracer-ics2023 <tracer@njuics.org>
2023-12-19 01:45:33 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:45:33 up  2:42,  1 user,  load average: 0.34, 0.27, 0.30

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -95,7 +95,9 @@ int NDL_Init(uint32_t flags) {
   gettimeofday(tv, NULL);
   int fd = open("/proc/dispinfo", 0, 0);
   char buf[128];
+  printf("fd:%d\n",fd);
   read(fd, buf, sizeof(buf));
+  printf("buf:%s\n",buf);
   sscanf(buf, "WIDTH:%d\nHEIGHT:%d", &screen_w, &screen_h);
   printf("screen_w = %d, screen_h = %d\n", screen_w, screen_h);
   //close(fd);
