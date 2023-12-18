c7aff39115d5edaf0037b7afd342a9149b3d1565
tracer-ics2023 <tracer@njuics.org>
2023-12-18 10:45:39 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:45:39 up 31 min,  1 user,  load average: 0.39, 0.38, 0.32

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -9,14 +9,10 @@ int main()
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
 	uint64_t ttime = 500;
-	printf("start\n");
-	printf("%llu\n", ttime);
 	while(1){
 		while((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ttime) gettimeofday(&tv, NULL);
-		printf("timer interrupt\n");
 		printf("tv_sec:%ld, tv_usec:%ld\n", tv.tv_sec, tv.tv_usec);
 		ttime += 500;
-		printf("%llu\n", ttime);
 	}
 	return 0;
 }
