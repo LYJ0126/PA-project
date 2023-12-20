a9fc84ad8eb3e1c3a4f71745a792b1c8cfc37df9
tracer-ics2023 <tracer@njuics.org>
2023-12-20 23:53:42 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:53:42 up  3:54,  1 user,  load average: 0.23, 0.42, 0.48

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -32,6 +32,7 @@ int SDL_WaitEvent(SDL_Event *event) {
   for(int i = 0; i < len; i++){
     if(strcmp(keyname[i], buf + 3) == 0){
       event->key.keysym.sym = i;
+      printf("event->key.keysym.sym:%d\n",event->key.keysym.sym)
       break;
     }
   }
