9183a57bce559fe7b7a7ec64eed9b8c7ce8ebdc6
tracer-ics2023 <tracer@njuics.org>
2023-12-21 00:04:03 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:04:03 up  4:04,  1 user,  load average: 0.75, 0.71, 0.58

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -22,24 +22,26 @@ int SDL_WaitEvent(SDL_Event *event) {
   char buf[20];
   memset(buf, 0, sizeof(buf));
   while(NDL_PollEvent(buf, 20) == 0);
-  printf("buf:%s\n",buf);
+  //printf("buf:%s\n",buf);
   if(buf[0]=='k' && buf[1]=='d') event->type = SDL_KEYDOWN;
   else if(buf[0]=='k' && buf[1]=='u') event->type = SDL_KEYUP;
   int len = sizeof(keyname) / sizeof(keyname[0]);
-  printf("len:%d\n",len);
+  //printf("len:%d\n",len);
   /*for(int i = 0; i < len; i++){
     printf("keyname[%d]:%s\n",i,keyname[i]);
   }*/
   for(int i = 0; i < len; i++){
-    printf("keyname[%d]:%s\n",i,keyname[i]);
+    /*printf("keyname[%d]:%s\n",i,keyname[i]);
     printf("buf + 3:%s\n",buf + 3);
     printf("strlen(keyname[i]):%d\n",strlen(keyname[i]));
     printf("strlen(buf + 3):%d\n",strlen(buf + 3));
-    printf("strcmp(keyname[i], buf + 3):%d\n",strcmp(keyname[i], buf + 3));
-    if(strcmp(keyname[i], buf + 3) == 0){
-      event->key.keysym.sym = i;
-      printf("event->key.keysym.sym:%d\n",event->key.keysym.sym);
-      break;
+    printf("strcmp(keyname[i], buf + 3):%d\n",strcmp(keyname[i], buf + 3));*/
+    if(strlen(keyname[i]) == strlen(buf + 3) - 1){//注意buf+3的长度比keyname[i]的长度多1
+      if(strcmp(keyname[i], buf + 3) == 0){
+        event->key.keysym.sym = i;
+        //printf("event->key.keysym.sym:%d\n",event->key.keysym.sym);
+        break;
+      }
     }
   }
 }
