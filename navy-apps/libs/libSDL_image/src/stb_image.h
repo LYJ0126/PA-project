88547f44f0ae350578bec11634ed465542a160f4
tracer-ics2023 <tracer@njuics.org>
2023-12-21 23:58:03 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:58:03 up  2:31,  1 user,  load average: 1.32, 0.86, 0.56

--- navy-apps/libs/libSDL_image/src/stb_image.h
+++ navy-apps/libs/libSDL_image/src/stb_image.h
@@ -928,8 +928,10 @@ STBIDEF stbi_us *stbi_load_16_from_memory(stbi_uc const *buffer, int len, int *x
 
 STBIDEF stbi_uc *stbi_load_from_memory(stbi_uc const *buffer, int len, int *x, int *y, int *comp, int req_comp)
 {
+   printf("stbi_load_from_memory\n");
    stbi__context s;
    stbi__start_mem(&s,buffer,len);
+   printf("stbi__load_and_postprocess_8bit\n");
    return stbi__load_and_postprocess_8bit(&s,x,y,comp,req_comp);
 }
 
