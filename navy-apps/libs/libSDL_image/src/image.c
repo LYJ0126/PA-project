3f5f1a2329f0b547ed1b6101d36cc155879a8605
tracer-ics2023 <tracer@njuics.org>
2023-12-21 18:04:42 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:04:42 up  6:12,  1 user,  load average: 0.52, 0.33, 0.33

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,7 +12,17 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  FILE *fp = fopen(filename, "r");
+  assert(fp != NULL);
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);//获取文件大小
+  fseek(fp, 0, SEEK_SET);
+  char *buf = malloc(size);
+  fread(buf, 1, size, fp);//将整个文件读入buf
+  fclose(fp);
+  SDL_Surface *ret = STBIMG_LoadFromMemory(buf, size);//从内存中加载图片
+  free(buf);
+  return ret;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
