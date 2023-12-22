3eb18a3f2f4bdac214834f84d039156125f55408
tracer-ics2023 <tracer@njuics.org>
2023-12-22 22:31:20 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:31:20 up  5:12,  1 user,  load average: 0.20, 0.12, 0.17

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -1,9 +1,11 @@
 #include <am.h>
 
 Area heap;
-
+extern int write(int fd,const void *buf,size_t cnt);
 void putch(char ch) {
+    write(1, (void*)&ch, 1);//利用系统调用进行输出
 }
 
 void halt(int code) {
+    //exit(code);//利用系统调用进行退出
 }
