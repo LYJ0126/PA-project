5a84db69590aeeaf07688926721ee3f63718bf44
tracer-ics2023 <tracer@njuics.org>
2023-12-23 02:34:04 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:34:04 up  9:14,  1 user,  load average: 0.41, 0.26, 0.21

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -23,12 +23,31 @@ static void sh_prompt() {
 }
 
 static void sh_handle_cmd(const char *cmd) {
+  printf("\n");
+  if(cmd[0] == '\n') return;
+  if (strcmp(cmd, "help") == 0) {
+    sh_printf("Built-in commands:\n");
+    sh_printf("  help: show this message\n");
+    sh_printf("  hello: say hello to the world\n");
+    sh_printf("  exit: exit the shell\n");
+  } else if (strcmp(cmd, "hello") == 0) {
+    sh_printf("Hello, world!\n");
+  } else if (strcmp(cmd, "exit") == 0) {
+    exit(0);
+  }
+  //获取文件名
+  char *args = strtok((char *)cmd, "\n");
+  if(args == NULL) return;
+  if(execvp(args, NULL) == -1) {
+    sh_printf("文件为找到\n");
+  }
+  return;
 }
 
 void builtin_sh_run() {
   sh_banner();
   sh_prompt();
-
+  setenv("PATH", "/bin", 0);
   while (1) {
     SDL_Event ev;
     if (SDL_PollEvent(&ev)) {
