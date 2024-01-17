26e83b74677f34f5689a26bc0614538e5cecb189
tracer-ics2023 <tracer@njuics.org>
2024-01-17 17:06:15 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:06:15 up  1:02,  1 user,  load average: 0.76, 0.36, 0.25

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -51,9 +51,9 @@ void init_proc() {
 
   Log("Initializing processes...");
   context_kload(&pcb[0], hello_fun, NULL);
-  //context_uload(&pcb[1],"/bin/menu",NULL,NULL);
+  context_uload(&pcb[1],"/bin/menu",NULL,NULL);
   //context_uload(&pcb[1],"/bin/exec-test",NULL,NULL);
-  context_uload(&pcb[1],"/bin/nterm",NULL,NULL);
+  //context_uload(&pcb[1],"/bin/nterm",NULL,NULL);
   switch_boot_pcb();
 
   // load program here
