68f5d619c74387dc8d6dd07071777b5cd7c59bf9
tracer-ics2023 <tracer@njuics.org>
2024-01-20 22:01:47 +0800

    >  compile NEMU 221220126 罗远兢 Linux lyj-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:01:47 up 18 min,  1 user,  load average: 0.49, 0.51, 0.39

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -40,10 +40,10 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, (void *)1L);
+  //context_kload(&pcb[0], hello_fun, (void *)1L);
   //context_kload(&pcb[1], hello_fun, (void *)2L);
   //Log("finish context_kload");
-  context_uload(&pcb[1], "/bin/nterm", NULL, NULL);
+  //context_uload(&pcb[1], "/bin/nterm", NULL, NULL);
   /*char *argv[] = {"--skip", NULL};
   context_uload(&pcb[1], "/bin/pal", argv, NULL);*/
   //Log("here Initializing processes...");
@@ -51,9 +51,9 @@ void init_proc() {
 
   Log("Initializing processes...");
   context_kload(&pcb[0], hello_fun, NULL);
-  //context_uload(&pcb[1],"/bin/menu",NULL,NULL);
+  context_uload(&pcb[1],"/bin/menu",NULL,NULL);
   //context_uload(&pcb[1],"/bin/exec-test",NULL,NULL);
-  context_uload(&pcb[1],"/bin/nterm",NULL,NULL);
+  //context_uload(&pcb[1],"/bin/nterm",NULL,NULL);
   switch_boot_pcb();
 
   // load program here
